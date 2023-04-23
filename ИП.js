84d9b7058e1f873414cf369675dd937d91439e48
Dimalep <114808407+Dimalep@users.noreply.github.com>
2023-04-23 09:20:48 +0500

    Add files via upload

---
+++ ИП.js
@@ -0,0 +1,32 @@
+
+document.getElementById("open_modal2").addEventListener("click", function(){
+    document.getElementById("modal2").classList.add("open2")
+})
+
+document.getElementById("close_modal2").addEventListener("click", function(){
+    document.getElementById("modal2").classList.remove("open2")
+})
+
+document.getElementById("open_modal3").addEventListener("click", function(){
+    document.getElementById("modal3").classList.add("open3")
+})
+
+document.getElementById("close_modal3").addEventListener("click", function(){
+    document.getElementById("modal3").classList.remove("open3")
+})
+
+document.getElementById("open_modal4").addEventListener("click", function(){
+    document.getElementById("modal4").classList.add("open4")
+})
+
+document.getElementById("close_modal4").addEventListener("click", function(){
+    document.getElementById("modal4").classList.remove("open4")
+})
+
+document.getElementById("open_modal5").addEventListener("click", function(){
+    document.getElementById("modal5").classList.add("open5")
+})
+
+document.getElementById("close_modal5").addEventListener("click", function(){
+    document.getElementById("modal5").classList.remove("open5")
+})
